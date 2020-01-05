5493c7e12d264a0b262f38b4c93ccfeb7ac48cb5
sekyidolma <44185851+sekyidolma@users.noreply.github.com>
2020-01-04 23:48:02 -0500

    Add files via upload

---
+++ countEven.java
@@ -0,0 +1,18 @@
+//Problem 6
+package BasicProblem;
+
+public class countEven {
+	public int countEven(int[] nums) {
+		if(nums==null || nums.length==0) {
+			return 0;
+		}
+		
+		int count=0;
+		for(int i =0; i <nums.length; i++) {
+			if(nums[i] %2==0) {
+				count++;
+			}
+		}
+		return count;
+	}
+}
